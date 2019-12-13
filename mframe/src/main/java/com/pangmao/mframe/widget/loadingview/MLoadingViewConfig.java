6fbb7c0a16c788184943bf6bcfe1f4d2a34961c3
wangmingzhi <wangmz199347@sina.com>
2019-12-13 15:59:58 +0800

    add

--- mframe/src/main/java/com/pangmao/mframe/widget/loadingview/MLoadingViewConfig.java
+++ mframe/src/main/java/com/pangmao/mframe/widget/loadingview/MLoadingViewConfig.java
@@ -8,6 +8,7 @@ public class MLoadingViewConfig {
 
     private int emptyViewResId = R.layout.xloading_empty_view;
     private int infoViewResId = R.layout.xloading_info_view;
+    private int imageViewResId = R.layout.xloading_image_view;
     private int errorViewResId = R.layout.xloading_error2_view;
     private int loadingViewResId = R.layout.xloading_loading_view;
     private int noNetworkViewResId = R.layout.xloading_no_network_view;
@@ -63,5 +64,13 @@ public class MLoadingViewConfig {
 
     public void setInfoViewResId(int infoViewResId) {
         this.infoViewResId = infoViewResId;
+    }
+
+    public int getImageViewResId() {
+        return imageViewResId;
+    }
+
+    public void setImageViewResId(int imageViewResId) {
+        this.imageViewResId = imageViewResId;
     }
 }

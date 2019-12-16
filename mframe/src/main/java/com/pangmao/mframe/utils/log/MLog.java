3229bce7ec32325d8b13dd48aef0d567cefc7320
wangmingzhi <wangmz199347@sina.com>
2019-12-16 16:07:21 +0800

    add

--- mframe/src/main/java/com/pangmao/mframe/utils/log/MLog.java
+++ mframe/src/main/java/com/pangmao/mframe/utils/log/MLog.java
@@ -5,9 +5,9 @@ import android.util.Log;
 
 import com.pangmao.mframe.MFrame;
 import com.pangmao.mframe.pojo.DateDifference;
-import com.pangmao.mframe.utils.MFileUtils;
 import com.pangmao.mframe.utils.MAppUtils;
 import com.pangmao.mframe.utils.MDateUtils;
+import com.pangmao.mframe.utils.MFileUtils;
 
 import java.io.File;
 import java.util.List;
@@ -84,7 +84,7 @@ public final class MLog {
         Log.d(MFrame.tag, msg);
         if (!MAppUtils.isApkInDebug()){
             String dataTime = MDateUtils.getCurrentDate("yyyy-MM-dd HH:mm:ss");
-            MFileUtils.write(dataTime + " " + msg, path,
+            MFileUtils.write(dataTime + ": " + msg, path,
                     fileName + MDateUtils.getCurrentDate("yyyyMMdd") + ".log");
         }
     }
@@ -101,12 +101,14 @@ public final class MLog {
             File[] files = file.listFiles();
             for (File f : files) {
                 String name = f.getName();
-                DateDifference dateDifference = MDateUtils.getTwoDataDifference(MDateUtils.string2Date(name.substring(0, 10), pattern));
-                boolean flag = name.length() == 19 && fileName.equalsIgnoreCase(name.substring(10, 19))
-                        && dateDifference.getDay() >= 10;
-                if (flag) {
-                    logger("删除的文件:" + name);
-                    MFileUtils.delFile(f);
+                if(name.length() == 17) {
+                    DateDifference dateDifference = MDateUtils.getTwoDataDifference(MDateUtils.string2Date(name.substring(5, 13), pattern));
+                    boolean flag = fileName.equalsIgnoreCase(name.substring(0, 5))
+                            && dateDifference.getDay() >= 10;
+                    if (flag) {
+                        logger("删除的文件:" + name);
+                        MFileUtils.delFile(f);
+                    }
                 }
             }
         }
